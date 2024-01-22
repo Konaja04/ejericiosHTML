1abaa136f8fd7d93d281348f24dbc133b35a9c4e
L34425 <L34425@lab.aca>
2024-01-22 12:10:00 -0500

    react2

--- Clases/prueba-react/src/ImagenCard.jsx
+++ Clases/prueba-react/src/ImagenCard.jsx
@@ -1,9 +1,18 @@
 
+import { useState } from 'react';
 import './ImagenCard.css';
 const ImagenCard = (props)=>{
-
+    const [color, setColor] = useState("0FFFFF")
+    const colorChange = {
+        backgroundColor: color
+    }
+    const cardOnClick = () =>{
+        if (color == "#0FFFFF")setColor("#FF10FF")
+        else setColor("#0FFFFF")
+    
+    }
     return <div>
-    <div className="estiloCard"> 
+    <div className="estiloCard" style={colorChange} onClick={cardOnClick}> 
         <img src={props.url} className="card-img-top" alt="" />
         <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
